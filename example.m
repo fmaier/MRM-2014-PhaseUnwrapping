5d0553e33443eeb2ea10334192be48d82f416972
Florian Maier <fmaier@mdanderson.org>
2014-04-10 19:15:49 -0500

    Added example code and data.

---
+++ example.m
@@ -0,0 +1,31 @@
+%% Load numerical phantom data.
+data = load('example.mat');
+
+%% Show data.
+figure;
+imagesc( abs(data.example) );
+title('Magnitude Image of Numerical Phantom');
+colormap gray;
+h = colorbar;
+set(get(h,'ylabel'),'String', 'Magnitude / a.u.');
+
+figure;
+imagesc( angle(data.example) );
+title('Phase Image of Numerical Phantom');
+colormap gray;
+h = colorbar;
+set(get(h,'ylabel'),'String', '\Phi / rad');
+
+%% Unwrap phase.
+tic;
+unwrappedPhase = sunwrap(data.example);         % No threshold.
+%unwrappedPhase = sunwrap(data.example, 0.25);  % Threshold: 25% of maximum value.
+toc;
+
+%% Show result.
+figure;
+imagesc( unwrappedPhase );
+title('Unwrapped Phase Image of Numerical Phantom');
+colormap gray;
+h = colorbar;
+set(get(h,'ylabel'),'String', '\Phi / rad');
